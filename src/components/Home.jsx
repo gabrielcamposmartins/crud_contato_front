050a98e030f0683714cdc2c093cd846b48d6c895
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-02-05 16:50:22 -0300

    criado componente Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Table from "./Table";
+import Form from "./Form";
+
+export default class Home extends Component {
+  table;
+  constructor(props) {
+    super(props);
+    this.refreshTable = this.refreshTable.bind(this);
+    this.setRef = this.setRef.bind(this);
+  }
+
+  refreshTable() {
+    this.table.renderViews();
+  }
+
+  setRef(ref) {
+    this.table = ref;
+  }
+
+  render() {
+    return (
+      <div>
+        <Form func={this.refreshTable} />
+        <Table ref={this.setRef} />
+      </div>
+    );
+  }
+}
